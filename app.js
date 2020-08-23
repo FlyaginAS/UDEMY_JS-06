0cd51a47137e1313eda6637a712d9f39ce7ff9c4
ruscat <rus.reg.36@gmail.com>
2020-08-23 15:09:34 +0300

    078

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
+//BUDGET CONTROLLER
 const budgetController = (function () {
-  const x = 23;
-  const add = function (a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function (b) {
-      return add(b);
-    },
-  };
+  //some code
 })();
 
+//UI CONTROLLER
 const UIController = (function () {
   //Some code
 })();
 
+//GLOBAL APP CONTROLLER
 const controller = (function (budgetCtrl, UICtrl) {
-  const z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: function () {
-      console.log(z);
-    },
+  const ctrlAddItem = function () {
+    //1) get the field input data
+    //2) add the item to the budget conroller
+    //3)add the item to the UI
+    //4) calculate the budget
+    //5) display the budget on the UI
+    console.log('it works');
   };
+  //some code
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
