437d9539bc351705d465548f321bc393151ad0cb
ruscat <rus.reg.36@gmail.com>
2020-08-23 14:16:38 +0300

    077

---
+++ app.js
@@ -0,0 +1,25 @@
+const budgetController = (function () {
+  const x = 23;
+  const add = function (a) {
+    return x + a;
+  };
+
+  return {
+    publicTest: function (b) {
+      return add(b);
+    },
+  };
+})();
+
+const UIController = (function () {
+  //Some code
+})();
+
+const controller = (function (budgetCtrl, UICtrl) {
+  const z = budgetCtrl.publicTest(5);
+  return {
+    anotherPublic: function () {
+      console.log(z);
+    },
+  };
+})(budgetController, UIController);
