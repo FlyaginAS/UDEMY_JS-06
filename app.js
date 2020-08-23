e2f8302b4db50526e110995e0e6f65b539b3d98c
ruscat <rus.reg.36@gmail.com>
2020-08-23 16:37:49 +0300

    081

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 //BUDGET CONTROLLER
 const budgetController = (function () {
-  //some code
+  const Expence = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  const Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
 })();
 
 //UI CONTROLLER
